0336b34eb71acef910dd2dbe8ba75b7c2f357f27
Matt Filetto <matt.filetto@gmail.com>
2013-12-29 04:26:30 -0800

    fix small derp

--- build-xbox.sh
+++ build-xbox.sh
@@ -17,6 +17,7 @@ echo "blacklist xpad" >> /etc/modprobe.d/blacklist.conf
 echo "installing lirc"
 sudo apt-get install -y lirc
 echo "lirc install complete"
+cd ../..
 cp -r hardware.conf /etc/lirc/
 cp -r lircd.conf /etc/lirc/
 echo "*** Drivers have now been compiled and installed, xpad driver blacklisted, configuration files copied, and lirc installed. Have a nice day ***"
